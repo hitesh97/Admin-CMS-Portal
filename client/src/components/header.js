3310f5fb94d738c9ee58b03e4e5bcac7b2450a5a
Chetan Singhal <chetan.singhal@impetus.co.in>
2018-07-05 13:20:49 +0530

    (fix): Toggle menu for smaller screen and preview will be scrollable with form scroll

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -63,7 +63,7 @@ class Header extends Component {
   render() {
     return (
 	  <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <a className="navbar-brand" href="#"><img src="../../images/Impetus_logo.png"/></a>
@@ -77,6 +77,9 @@ class Header extends Component {
             <a className="nav-link text-center" id="sidenavToggler">
               <i className="fa fa-fw fa-angle-left"></i>
             </a>
+            <ul class="sidenav-second-level collapse" id="collapseExamplePages">
+              {this.topNav()}
+            </ul>            
           </li>
         </ul>    
         <ul className="navbar-nav ml-auto">
